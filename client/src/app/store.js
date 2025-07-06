d8560a7a1c2f5db0e479206fec9df40485cd2676
supaKd <kevin.khek@gmail.com>
2025-07-06 20:51:16 +0200

    Met à jour les URLs API

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authslice";
+import authReducer from "../features/authSlice";
 import menuReducer from "../features/menuSlice";
 import cartReducer from "../features/cartSlice";
 
