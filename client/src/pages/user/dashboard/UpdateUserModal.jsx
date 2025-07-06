c16ea2ed79e676fdfe3f6232e5397aaaba09cead
supaKd <kevin.khek@gmail.com>
2025-07-06 20:37:13 +0200

    Met à jour

--- client/src/pages/user/dashboard/UpdateUserModal.jsx
+++ client/src/pages/user/dashboard/UpdateUserModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../../../features/authslice";
+import { login } from "../../../features/authSlice";
 import { useNavigate } from "react-router-dom";
 
 function UpdateUserModal({ userInfos, onClose }) {
