d77837b14ee3386b1684001543659d1798a09c9c
supaKd <kevin.khek@gmail.com>
2025-07-10 16:42:00 +0200

    ajout

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -2,11 +2,12 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
-  	port: process.env.DB_PORT, // Spécifie le port MySQL si différent (ex: MAMP, Homebrew sur Mac)
+	port: parseInt(process.env.DB_PORT, 10), 
 	database: process.env.DB_NAME,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 });
+
 
 pool.getConnection()
 	.then((res) => console.log(`Connected to ${res.config.database} database`))
