e778b9332e741b07a85825493fa80b12f23b0655
supaKd <kevin.khek@gmail.com>
2025-07-10 16:55:55 +0200

    ajout

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,14 @@ import Hero from "../home/Hero";
 import Product from "../home/Product";
 import Category from "./Category";
 
+useEffect(() => {
+  fetch("https://ksushiv1.onrender.com/ping")
+    .then(res => res.text())
+    .then(console.log)
+    .catch(console.error);
+}, []);
+
+
 function Home() {
   const [selectedCategory, setSelectedCategory] = useState("all");
 
